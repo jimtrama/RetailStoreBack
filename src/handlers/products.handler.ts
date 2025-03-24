4da99299265a4d0b1ac60b546972b4558632e9f7
Dimitris Tramantzas <36377889+jimtrama@users.noreply.github.com>
2025-03-24 19:25:15 +0200

    pr

--- src/handlers/products.handler.ts
+++ src/handlers/products.handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Product from "../schemas/product.schema";
+import { validationResult } from "express-validator";
 
 export async function getAll(req: Request, res: Response) {
   try {
@@ -12,8 +13,14 @@ export async function getAll(req: Request, res: Response) {
 
 export async function getProduct(req: Request, res: Response) {
   try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(300).json({ errors: result.array() });
+      return;
+    }
+    
     const id = req.headers["id"];
-    const products = await Product.find({_id:id});
+    const products = await Product.find({ _id: id });
     res.status(200).json({ products });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
@@ -22,6 +29,11 @@ export async function getProduct(req: Request, res: Response) {
 
 export async function createProduct(req: Request, res: Response) {
   try {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(300).json({ errors: result.array() });
+      return;
+    }
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error: any) {
@@ -31,20 +43,20 @@ export async function createProduct(req: Request, res: Response) {
 
 export async function updateProduct(req: Request, res: Response) {
   try {
-    const id = req.body["id"];
-    const values = req.body["values"];
-
-    if (!id || !values) {
-      res.status(300).json({ message: `id and values must be present` });
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(300).json({ errors: result.array() });
       return;
     }
+    const id = req.headers["id"];
+    const values = req.body;
 
     if (Object.keys(values)?.includes("_id")) {
       res.status(300).json({ message: `sorry id shoudn't be updated` });
       return;
     }
 
-    const product = await Product.findOneAndUpdate({ _id:id }, values);
+    const product = await Product.findOneAndUpdate({ _id: id }, values);
     if (!product) {
       res.status(300).json({ message: `product with id ${id} not found` });
       return;
@@ -57,12 +69,13 @@ export async function updateProduct(req: Request, res: Response) {
 
 export async function deleteProduct(req: Request, res: Response) {
   try {
-    const id = req.body["id"];
-    if (!id) {
-      res.status(300).json({ message: `id must be present` });
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(300).json({ errors: result.array() });
       return;
     }
-    const product = await Product.findOneAndDelete({ _id:id });
+    const id = req.headers["id"];
+    const product = await Product.findOneAndDelete({ _id: id });
     if (!product) {
       res.status(300).json({ message: `product with id ${id} not found` });
       return;
--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import { createProduct, deleteProduct, getAll, getProduct, updateProduct } from "../handlers/products.handler";
+import { body, header } from "express-validator";
 const router = Router();
 
 router.get("/",getAll);
-router.get("/one",getProduct);
-router.post("/new",createProduct);
-router.put("/update",updateProduct);
-router.delete("/delete",deleteProduct);
+router.get("/one",
+    header('id').notEmpty(),
+    getProduct);
+router.post("/new",
+    body('title').notEmpty(),
+    body('price').notEmpty().isInt({min:0}),
+    createProduct);
+router.put("/update",
+    header('id').notEmpty(),
+    body('stock').if(body('stock').notEmpty()).isInt({min:0}),
+    body('').notEmpty(),
+    updateProduct);
+router.delete("/delete",
+    header('id').notEmpty(),
+    deleteProduct);
 
 export default router;
